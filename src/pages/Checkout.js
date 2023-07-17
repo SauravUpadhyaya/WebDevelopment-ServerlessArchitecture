671c40a58598abcaf2e4b8d2ac5798db54463d66
Saurav <saurav@Sauravs-MacBook-Pro.local>
2023-07-17 01:23:27 -0500

    added logic

---
+++ src/pages/Checkout.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { AmplifyAuthenticator } from '@aws-amplify/ui-react';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+import CheckoutForm from "../components/CheckoutForm";
+
+const Checkout = () => {
+    const stripePromise = loadStripe('pk_test_51Mr5lUFiXeHFpWskbo0a6QUeDide3JVOMW4yYoQ6PAVsljgHPWP2mZz2dNqrWhozC5q1egQSPkLBPJ0PT7SV4ocS00fQnhVnm2');
+
+    return (
+        <section className="checkout-wrapper">
+            <AmplifyAuthenticator>
+                <Elements stripe={stripePromise}>
+                    <section>
+                        <h2>Time to Checkout?</h2>
+                        <CheckoutForm />
+                    </section>
+                </Elements>
+            </AmplifyAuthenticator>
+        </section>
+    )
+}
+
+export default Checkout
